95a3422638d6d41ec0a2d13b85b37e165e7bce6a
Matt Bow <mattybow@gmail.com>
2015-12-17 17:17:09 -0600

    add date

--- app/components/tweetListItem.js
+++ app/components/tweetListItem.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 var TweetListItem = (props) => {
   const {text, link, created_at} = props.tweet;
+  const time = moment(new Date(created_at)).format('lll');
   return <li className="tweet-list-item">
     <a href={link}>
       <div>{text}</div>
+      <div style={{color:"#A06CD5"}}>{time}</div>
     </a>
   </li>;
 };
