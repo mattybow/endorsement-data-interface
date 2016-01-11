6be62b330eea4ddc037904a71d5c42492ef374af
Matt Bow <mattybow@gmail.com>
2016-01-11 16:48:25 -0600

    debugging

--- server/mysqlTransaction.js
+++ server/mysqlTransaction.js
@@ -9,6 +9,7 @@ class MySqlTransaction {
     return new Promise(resolve => {
       this.connection.query(query, (err,rows)=>{
         if(err) {
+          console.log(err);
           resolve({code:500,ok:false,err});
         } else {
           resolve({code:201,ok:true,rows});
